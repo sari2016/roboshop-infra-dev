5b6174f4cdc21c2dec3d76e637c6944eeadb08a5
sari2016 <sarithagovindaraju@gmail.com>
2025-11-25 20:36:22 -0600

    sg

---
+++ 10-sg/sg.tf
@@ -0,0 +1,21 @@
+module "mysql_sg" {
+    source = "git::https://github.com/sari2016/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mysql"
+    sg_description = "Created for MySQL instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+
+
+module "bastion_sg" {
+    source = "git::https://github.com/sari2016/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "bastion"
+    sg_description = "Created for bastion instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
